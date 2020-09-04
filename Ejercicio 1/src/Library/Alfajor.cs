f36c52aa37c6c13bbb78557fd8ebf865ec3631fc
LautaroDaRosa <LautaroDaRosa@users.noreply.github.com>
2020-09-04 16:53:38 -0300

    ejercicio 2

--- Ejercicio 1/src/Library/Alfajor.cs
+++ Ejercicio 1/src/Library/Alfajor.cs
@@ -1,9 +1,6 @@
 using System;
 
 namespace Expert_SRP
-//Cumple con el principio Expert ya que posee la información necesaria para cumplir la responsabilidad que tiene que es 
-//crear el objeto(alfajor), pero, no cumple con el principio SRP ya que deberían estar encapsuladas las variables 
-//precioDulce y precioMasa
 {
     public class Alfajor 
     {
