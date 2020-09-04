c63531787eeaf645b58563b3acd15fceb0bc2ddb
LautaroDaRosa <LautaroDaRosa@users.noreply.github.com>
2020-09-04 16:30:30 -0300

    ejercicio 1

--- Ejercicio 1/src/Library/Kiosco.cs
+++ Ejercicio 1/src/Library/Kiosco.cs
@@ -1,6 +1,11 @@
 using System;
 
 namespace Expert_SRP
+//No cumple con el principio SRP porque posee más de una responsabilidad, dentro del objeto kiosco, debe comprobar si 
+//si puede comprar(PuedeComprar) y hacer conversión de moneda(ConvertirAPesos), además al poseer un valor que varía como
+//es el del dólar en relación al peso la clase tendría que cambiar más de una vez ya que el valor del dólar cambia 
+//constantemente, tampoco cumple con el principio expert porque requiere información de la clase alfajor que la misma
+//clase debería utilizar para hacer el cálculo, ya que es esa clase la que posee la información
 {
     public class Kiosco 
     {
