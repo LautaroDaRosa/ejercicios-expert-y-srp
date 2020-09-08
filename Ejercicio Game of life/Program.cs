e2a18e357651abdffe1b9f90421b216bb085f486
LautaroDaRosa <LautaroDaRosa@users.noreply.github.com>
2020-09-08 19:47:15 -0300

    ejercicio game of life

---
+++ Ejercicio Game of life/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_Game_of_life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
